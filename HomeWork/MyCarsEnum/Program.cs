33df2d17c0a1ce238564a4c219ab46172cf74681
Anton <https://github.com/AntonSadkov/C_Sharp.git>
2023-01-22 21:06:43 +0300

    Программа которая выводит автомобили, которые были у меня во владении.

---
+++ HomeWork/MyCarsEnum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MyCarsEnum
+{
+    /*
+     * Программа которая выводит автомобили, которые были у меня во владении.
+     */
+    public class MyCarsEnum
+    {
+        enum MyCars
+        {
+            Vaz_2110 = 1,
+            Opel_Astra_Body_H,
+            Mazda3_Body_BK,
+            Volkswagen_Golf_GTI_Body_MK6
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите от 1, до 4, чтобы узнать какие машины у меня были:");
+
+            int numberCars = int.Parse(Console.ReadLine());
+
+            if(numberCars <= 4)
+            {
+                MyCars myCars = (MyCars)numberCars;
+                Console.WriteLine(myCars.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Введенно некорректное значение");
+            }           
+        }
+    }
+}
